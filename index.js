d6a4426c3b7871a9a4d19516fd12b110fbfabf7f
Amberzum <108992156+Amberzum@users.noreply.github.com>
2022-09-03 18:05:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let password1 = document.getElementById("password-1");
+let password2 = document.getElementById("password-2");
+
+function generatePasswords() {
+  let pw1 = "";
+  let pw2 = "";
+
+  for (let i = 0; i < 10; i++) {
+    let randomChar1 = characters[Math.floor(Math.random() * characters.length)];
+    let randomChar2 = characters[Math.floor(Math.random() * characters.length)];
+
+    pw1 = pw1 + randomChar1;
+    pw2 = pw2 + randomChar2;
+  }
+
+  password1.innerText = pw1;
+  password2.innerText = pw2;
+}
